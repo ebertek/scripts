54926d94b48161e29e4650998291b2ddb3628225
David Ebert <ebertek@gmail.com>
2016-06-18 02:02:33 +0200

    macOS Sierra

--- OSX-Beta.sh
+++ OSX-Beta.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 ### Developer
 ## 10.9 Mavericks
-# https://swscan.apple.com/content/catalogs/others/index-10.9seed-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz
+MAV="https://swscan.apple.com/content/catalogs/others/index-10.9seed-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
 ## 10.10 Yosemite
-# https://swscan.apple.com/content/catalogs/others/index-10.10seed-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz
+YOS="https://swscan.apple.com/content/catalogs/others/index-10.10seed-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
 ## 10.11 El Capitan
-# https://swscan.apple.com/content/catalogs/others/index-10.11seed-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz
+CAP="https://swscan.apple.com/content/catalogs/others/index-10.11seed-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
+## 10.12 Sierra
+SIE="https://swscan.apple.com/content/catalogs/others/index-10.12seed-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
 
 ### Public AppleSeed:
 # https://swscan.apple.com/content/catalogs/others/index-10.9publicseed-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz
@@ -19,4 +21,4 @@ defaults read /Library/Preferences/com.apple.SoftwareUpdate.plist CatalogURL
 # sudo /usr/sbin/softwareupdate --clear-catalog
 
 ### Set URL
-sudo /usr/sbin/softwareupdate --set-catalog "https://swscan.apple.com/content/catalogs/others/index-10.11seed-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
+sudo /usr/sbin/softwareupdate --set-catalog "$SIE"
