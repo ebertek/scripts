cf55ba3737ad60b0b3221d7123505a39f9039d61
David Ebert <ebertek@gmail.com>
2016-07-11 22:45:09 +0200

    Add Sierra public beta

--- OSX-Beta.sh
+++ OSX-Beta.sh
@@ -13,6 +13,7 @@ SIE="https://swscan.apple.com/content/catalogs/others/index-10.12seed-10.12-10.1
 # https://swscan.apple.com/content/catalogs/others/index-10.9publicseed-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz
 # https://swscan.apple.com/content/catalogs/others/index-10.10beta-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz
 # https://swscan.apple.com/content/catalogs/others/index-10.11beta-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz
+# https://swscan.apple.com/content/catalogs/others/index-10.12beta-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz
 
 ### Get current URL
 defaults read /Library/Preferences/com.apple.SoftwareUpdate.plist CatalogURL
