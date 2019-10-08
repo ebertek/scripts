3620fa135e26723fbfa408e8eb20c5ad5afb1603
David Ebert <ebertek@gmail.com>
2019-10-08 19:39:58 +0200

    Create sdcard.sh

---
+++ sdcard.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo kextunload /System/Library/Extensions/AppleStorageDrivers.kext/Contents/PlugIns/AppleUSBCardReader.kext; sudo kextload /System/Library/Extensions/AppleStorageDrivers.kext/Contents/PlugIns/AppleUSBCardReader.kext
